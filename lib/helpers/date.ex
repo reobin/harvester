e9d027ac6a3ac7268e9270303e56152d97a13dd6
Robin Gagnon <me@reobin.dev>
2022-11-26 09:03:45 -0500

    feat: Print current month after hours

--- lib/helpers/date.ex
+++ lib/helpers/date.ex
@@ -18,4 +18,18 @@ defmodule Harvester.Helpers.Date do
 
   @spec format(Date.t()) :: String.t()
   defp format(date), do: date |> NativeDate.to_iso8601() |> String.slice(0..9)
+
+  @doc """
+  Get current month formatted in yyyy-mm
+  """
+  @spec get_current_month() :: String.t()
+  def get_current_month do
+    NativeDate.utc_today()
+    |> NativeDate.to_string()
+    |> String.split("-")
+    |> Enum.reverse()
+    |> then(&tl/1)
+    |> Enum.reverse()
+    |> Enum.join("-")
+  end
 end
