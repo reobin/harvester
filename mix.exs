b37427affb4c63720d83fb41725cd64307140b09
Robin Gagnon <me@reobin.dev>
2022-11-25 19:05:20 -0500

    feat: Set up dotenv

---
+++ mix.exs
@@ -0,0 +1,26 @@
+defmodule Harvester.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :harvester,
+      version: "1.0.0",
+      elixir: "~> 1.14",
+      deps: [
+        {:dotenv, "~> 3.0.0"},
+        {:httpoison, "~> 1.8.2"}
+      ],
+      aliases: aliases()
+    ]
+  end
+
+  def application do
+    [extra_applications: [:logger]]
+  end
+
+  defp aliases do
+    [
+      "harvester.run": "run bin/run"
+    ]
+  end
+end
