ca7a98052733493691051ba3cea214c6cfbbf437
Robin Gagnon <me@reobin.dev>
2022-11-25 21:10:36 -0500

    feat: Aggregate all current month hours

--- lib/modules/harvest.ex
+++ lib/modules/harvest.ex
@@ -5,12 +5,11 @@ defmodule Harvester.Modules.Harvest do
 
   alias Harvester.Modules.HTTP
 
-  alias Harvester.Helpers.Map, as: MapHelper
-
   @harvest_api_url "https://api.harvestapp.com/v2"
 
   @type auth :: %{access_token: String.t(), account_id: String.t()}
   @type user :: %{id: integer(), first_name: String.t(), last_name: String.t(), email: String.t()}
+  @type time_entry :: %{hours: float()}
 
   @doc """
   Returns a user's Harvest account information.
@@ -24,19 +23,23 @@ defmodule Harvester.Modules.Harvest do
   @doc """
   Returns all time entries based on parameters.
   """
-  @spec list_time_entries!(auth, map()) :: [map()]
+  @spec list_time_entries!(auth, map()) :: [time_entry]
   def list_time_entries!(auth, options \\ []) do
-    default_options = %{user_id: nil, from: "2022-01-01", to: "2023-01-01"}
+    default_options = %{user_id: nil, from: "2022-01-01", to: "2023-01-01", page: 1}
     params = Enum.into(options, default_options)
 
     headers = build_headers(auth)
 
-    "#{@harvest_api_url}/time_entries"
-    |> HTTP.get!(headers, params)
-    |> Map.get(:time_entries, [])
+    result = "#{@harvest_api_url}/time_entries" |> HTTP.get!(headers, params)
+
+    if result.total_pages > result.page do
+      result.time_entries ++ list_time_entries!(auth, Map.put(params, :page, result.page + 1))
+    else
+      result.time_entries
+    end
   end
 
-  @spec build_headers(auth) :: list()
+  @spec build_headers(auth) :: list({String.t(), String.t()})
   defp build_headers(auth),
     do: [
       {"Harvest-Account-Id", auth.account_id},
