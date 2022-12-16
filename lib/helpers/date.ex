62915e542f4d87c68ec70d0c91e4fb40b5ff3918
Robin Gagnon <me@reobin.dev>
2022-12-15 23:49:40 -0500

    feat: Add option to define month through CLI arguments

--- lib/helpers/date.ex
+++ lib/helpers/date.ex
@@ -3,15 +3,13 @@ defmodule Harvester.Helpers.Date do
   Helper functions for working with dates.
   """
 
-  alias Date, as: NativeDate
-
   @doc """
   Returns the month's first and last days.
   """
   @spec get_month_range(Date.t()) :: {Date.t(), Date.t()}
   def get_month_range(date) do
-    first_day = date |> NativeDate.beginning_of_month() |> format()
-    last_day = date |> NativeDate.end_of_month() |> format()
+    first_day = date |> Date.beginning_of_month() |> format()
+    last_day = date |> Date.end_of_month() |> format()
 
     {first_day, last_day}
   end
@@ -21,10 +19,10 @@ defmodule Harvester.Helpers.Date do
   """
   @spec get_previous_month() :: Date.t()
   def get_previous_month do
-    NativeDate.utc_today()
-    |> NativeDate.beginning_of_month()
-    |> NativeDate.add(-1)
-    |> NativeDate.beginning_of_month()
+    Date.utc_today()
+    |> Date.beginning_of_month()
+    |> Date.add(-1)
+    |> Date.beginning_of_month()
   end
 
   @doc """
@@ -32,6 +30,21 @@ defmodule Harvester.Helpers.Date do
   """
   @spec format(Date.t()) :: String.t()
   def format(date), do: format(date, :long)
-  def format(date, :short), do: date |> NativeDate.to_iso8601() |> String.slice(0..6)
-  def format(date, :long), do: date |> NativeDate.to_iso8601() |> String.slice(0..9)
+  def format(date, :short), do: date |> Date.to_iso8601() |> String.slice(0..6)
+  def format(date, :long), do: date |> Date.to_iso8601() |> String.slice(0..9)
+
+  @doc """
+  Gets date from system arguments.
+  """
+  @spec from_args() :: Date.t() | nil
+  def from_args() do
+    try do
+      [year, month] =
+        System.argv() |> Enum.at(0) |> String.split("/") |> Enum.map(&String.to_integer/1)
+
+      Date.new!(year, month, 1)
+    rescue
+      _ -> nil
+    end
+  end
 end
